663614a96f2b48c4d1e4ef325f21ed4a9c2448ae
Himanshu Dewangan <himanshu.dewangan@csm.tech>
2026-04-02 15:49:04 +0530

    project section fixed

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,58 @@
-import {AfterViewInit, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+   
 
+  projects = [
+    {
+      name: 'Cake Shop',
+      description: 'Multi-outlet cake shop system with admin dashboard.',
+      tech: ['HTML', 'CSS', 'JS', 'Node'],
+      image: '/cake.jpg',
+      live: '#',
+      github: '#'
+    },
+    {
+      name: 'E-commerce',
+      description: 'Scalable store with payments & authentication.',
+      tech: ['React', 'Firebase'],
+      image: '/ecom.jpg',
+      live: '#',
+      github: '#'
+    },
+    {
+      name: 'Restaurant System',
+      description: 'Complete restaurant management solution.',
+      tech: ['PHP', 'MySQL'],
+      image: '/resto.jpg',
+      live: '#',
+      github: '#'
+    },
+    {
+      name: 'Restaurant System',
+      description: 'Complete restaurant management solution.',
+      tech: ['PHP', 'MySQL'],
+      image: '/resto.jpg',
+      live: '#',
+      github: '#'
+    },
+    {
+      name: 'Restaurant System',
+      description: 'Complete restaurant management solution.',
+      tech: ['PHP', 'MySQL'],
+      image: '/resto.jpg',
+      live: '#',
+      github: '#'
+    }
+  ];
+
+ 
 }
